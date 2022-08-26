000d0e127d87b52b1076a0637a9f59829932d1ba
Poul Igorson <pouligorson@mail.ru>
2022-08-26 19:47:21 +0300

    Update views

--- main/views.py
+++ main/views.py
@@ -4,10 +4,13 @@ from datetime import date
 
 
 def index_page(request):
-    return render(
-        request,
-        'index.html', {
-            'pagename': 'Главная',
+    return render(request, 'index.html', {'pagename': 'Главная'})
+
+
+def today_json(request):
+    return {
+        'status': 200,
+        'body': {
             'today': f'{date.today()}'
         }
-    )
+    }
