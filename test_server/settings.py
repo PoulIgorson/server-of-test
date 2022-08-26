6e92710b7a0dfdf1e78650af3c3b23209a61f031
Poul Igorson <pouligorson@mail.ru>
2022-08-26 19:02:36 +0300

    Fix settings

--- test_server/settings.py
+++ test_server/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-0ncgagq(o1l=l*wplm0y#qxtp-^e1w4$y+(_i2kl=+hke22-=&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
