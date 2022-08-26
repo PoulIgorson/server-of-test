56a2b4e74d036fa3ee6037678f9ecadf59c86ba4
Poul Igorson <pouligorson@mail.ru>
2022-08-26 19:44:16 +0300

    Update views

--- main/views.py
+++ main/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 
+from datetime import date
+
 
 def index_page(request):
-    return render(request, 'index.html', {'pagename': 'Главная'})
+    return render(
+        request,
+        'index.html', {
+            'pagename': 'Главная',
+            'today': f'{date.today()}'
+        }
+    )
